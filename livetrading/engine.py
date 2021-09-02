08cd1bb8460ee65b9ed16dad99c80e4b3ee7fb2a
Fabio Gomez <fabiogomez11c@gmail.com>
2021-09-02 18:05:26 -0500

    first connection done

--- livetrading/engine.py
+++ livetrading/engine.py
@@ -9,4 +9,5 @@ class LiveTrading:
             "35IB27RflS7T0vm5CDi3b3vVEx4lVdtTB2lMP0WszCmF4GiGFOdbZVufIJtrcXeZ"
         )
 
-        self.depth = self.client.get_order_book(symbol='BTCUSD')
+        self.depth = self.client.get_all_tickers()
+
--- main_live.py
+++ main_live.py
@@ -3,3 +3,5 @@ from livetrading.engine import LiveTrading
 if __name__ == "__main__":
     
     engine = LiveTrading()
+
+    print('Done')
