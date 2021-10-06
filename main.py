15418ca82d0d7907da33ed0d005a9c1d51cfd929
Fabio Gomez <fabiogomez11c@gmail.com>
2021-10-06 11:25:18 -0500

    main fixed

--- main.py
+++ main.py
@@ -15,25 +15,25 @@ if __name__ == '__main__':
     Enjoy!
     """)
 
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('--pairset', action='store')
-    # parser.add_argument('--datestart', action='store')
-    # parser.add_argument('--dateend', action='store')
-    # parser.add_argument('--candle', action='store')
-    # args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pairset', action='store')
+    parser.add_argument('--datestart', action='store')
+    parser.add_argument('--dateend', action='store')
+    parser.add_argument('--candle', action='store')
+    args = parser.parse_args()
 
 
-    # from_date = args.datestart
-    from_date = '2021-01-01'
+    from_date = args.datestart
+    # from_date = '2021-01-01'
 
-    # to_date = args.dateend
-    to_date = '2021-04-21'
+    to_date = args.dateend
+    # to_date = '2021-04-21'
 
-    # time_frame = args.candle
-    time_frame = '4h'
+    time_frame = args.candle
+    # time_frame = '4h'
 
-    # symbol = args.pairset
-    symbol = 'LINK/USD'
+    symbol = args.pairset
+    # symbol = 'LINK/USD'
 
     if from_date is None or to_date is None or time_frame is None or symbol is None:
         raise NotImplementedError('One or more parameters were not defined')
@@ -42,6 +42,6 @@ if __name__ == '__main__':
     engine = Backtester(from_date=from_date, to_date=to_date, time_frame=time_frame, symbol=symbol)
     engine.run_backtest()
 
-    # engine.create_report()
+    engine.create_report()
 
     print('Done')
