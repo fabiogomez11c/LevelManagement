1a92c6a56f4d71103eb674cb1f22e984ad75aff8
Fabio Gomez <fabiogomez11c@gmail.com>
2021-10-06 13:21:27 -0500

    market message for live trading

--- livetrading/engine.py
+++ livetrading/engine.py
@@ -17,6 +17,8 @@ class LiveTrading:
         self.base = dict((i, i[-3::]) for i in self.symbol)
         # self.base = symbol[3::]
         self.quantity = baseQuantity
+
+        open('./livetrading/log.txt', 'w').close()
         
         self.client = binance.Client(
             "ifWJ8ujCxzYk6Kuo6tgrUGZj905BeOhRHH2DKhtjUazETm5jGvPFaeXuaMn281To",
@@ -38,6 +40,26 @@ class LiveTrading:
         # create the strategy handler
         self.strategy = Strategy(self)
     
+    def _log(self, message, symbol):
+        timestamp = self.data.data[symbol].iloc[-1:].index[0]
+        finalMessage = str(timestamp) + ' | ' + message
+
+        print(finalMessage)
+
+        with open('./livetrading/log.txt', 'a') as f:
+            f.write(finalMessage + '\n')
+            f.close()
+
+    def _market_message(self, symbol):
+
+        message = ''
+
+        bars = self.data.data[symbol].iloc[-1:].to_numpy()[0]
+        # bars = self.data.get_latest_bars(1)[-1]
+        message = f'{symbol} | Open: {bars[0]}, High: {bars[1]}, Low: {bars[2]}, Close: {bars[3]}, c/r fib: {bars[6]}, c/r fast: {bars[7]}, fib_pred: {bars[8]}, fast_pred: {bars[9]}, fast_ma: {bars[10]}, slow_pred: {bars[11]}, high_pred: {bars[12]}'
+
+        return message
+
     def _update_last_datetime(self, symbol):
         self.last_datetime = self.data.data[symbol].iloc[-1].name
     
@@ -60,13 +82,15 @@ class LiveTrading:
                     if self._check_last_datetime(s):
                         continue
                     else:
+                        # print(str(self.data.data[s].iloc[-1:].index[0]))
+                        # print(self.data.data[s].iloc[-1:].to_numpy())
                         break
                 
             # loop for symbols
             for s in self.symbol:
 
-                print(f'Symbol: {s} | Last datetime:', self.data.data[s].iloc[-1].name)
                 self._update_last_datetime(s)
+                self._log(self._market_message(s), s)
 
                 # get the existing positions
                 contra = float(self.client.get_asset_balance(asset=self.contra[s])['free'])
